c3afc1ed033e9384be54f861d845951a5825aa1b
Stephanierlandry <stephanierlandry@gmail.com>
2019-12-02 15:01:36 -0600

    CORS specified to only allow certain origins access

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const cors = require('cors');
-app.use(cors());
 
 const mongoose = require('mongoose');
 const Models = require('./models.js');
@@ -25,6 +23,22 @@ app.use(morgan('common'));
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
+
+const cors = require('cors');
+app.use(cors());
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const { check, validationResult } = require('express-validator');
 
