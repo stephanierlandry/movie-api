3eb08eaec6c7cf4c5e22583d0db295224ee4ed6a
Stephanierlandry <stephanierlandry@gmail.com>
2019-12-06 11:16:11 -0600

    JWTStrategy testing

--- passport.js
+++ passport.js
@@ -31,7 +31,7 @@ passport.use(new LocalStrategy({
 }));
 
 passport.use(new JWTStrategy({
- jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+ jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
  secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
  return Users.findById(jwtPayload._id)
