c7f16529f48e5f5201d2367a4699ab2aaef42cd5
Stephanierlandry <stephanierlandry@gmail.com>
2019-12-15 15:07:57 -0600

    adding the logic to get movies from the database

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
-  constructor() {
-    // Call the superclass constructor
-    // so React can initialize it
-    super();
+export class MainView extends React.Component {
 
-    // Initialize the state to an empty object so we can destructure it later
-    this.state = {};
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('<https://design-and-a-movie.herokuapp.com/movies>')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
-  // This overrides the render() method of the superclass
-  // No need to call super() though, as it does nothing by default
   render() {
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
+    const { movies } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
     return (
-     <div className="main-view"></div>
+     <div className="main-view">
+     { movies.map(movie => (
+       <div className="movie-card" key={movie._id}>{movie.Title}</div>
+     ))}
+     </div>
     );
   }
 }
