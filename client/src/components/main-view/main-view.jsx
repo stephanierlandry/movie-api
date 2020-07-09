2a24da85393e362e0abfec4e000949a6727996c0
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-09 12:46:59 -0500

    profile update view route path changed to "/"

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -200,7 +200,7 @@ export class MainView extends React.Component {
                         );
                       }}/>
 
-              <Route exact path="/update-user/:username"
+              <Route exact path="/"
                       render={() => {
                         return (
                           <ProfileUpdateView user={user} userProfile={userProfile} movies={movies}/>
